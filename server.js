63811fca4071239f58ea24969ce7256d47edcbf9
ElvioAbduction <109813828+ElvioAbduction@users.noreply.github.com>
2023-03-14 09:17:05 +0100

    second commit

--- server.js
+++ server.js
@@ -1,9 +1,55 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
+
+const database = {
+	users: [
+	{
+		id: '123',
+		name: 'John',
+		email: 'elviocorona@mail.com',
+		password: 'cookies',
+		entries: 0,
+		joined: new Date()
+	},
+	{
+		id: '234',
+		name: 'Sally',
+		email: 'sally@mail.com',
+		password: 'banana',
+		entries: 0,
+		joined: new Date()
+	}
+  ]
+}
+
 
 app.get('/', (req , res) => {
-	res.send('this is working');
+	res.send(database.users);
+})
+
+
+app.post('/signin',(req, res) => {
+	if (req.body.email === database.users[0].email &&
+		req.body.password === database.users[0].password){
+		res.json('success');
+	}else{
+		res.status(400).json('user not found');
+	}
+})
+
+
+app.post('/register',(req, res) => {
+	database.users.push({
+		id: '345',
+		name: req.body.name,
+		email: req.body.email,
+		password: req.body.password,
+		entries: 0,
+		joined: new Date()
+	})
+	res.json(database.users[database.users.length-1])
 })
 
 
